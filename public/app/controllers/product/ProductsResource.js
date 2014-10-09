bb6a53cc0f5d72840720d4e057d21363127eff80
TCVD <viktordakov97@gmail.com>
2014-10-09 23:01:16 +0300

    Added resource and get of all products

--- public/app/controllers/product/ProductsResource.js
+++ public/app/controllers/product/ProductsResource.js
@@ -1,6 +1,8 @@
 /**
  * Created by Виктор on 9.10.2014 г..
  */
-app.factory('ProductsResource'['$http', function($resource) {
+app.factory('ProductsResource', function($resource) {
+    var ProductsResource = $resource('api/products');
+
     return CourseResource;
-}])
+})
