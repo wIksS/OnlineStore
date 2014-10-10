036b8f42bcc9d255ba841208dfd63649511b4256
svetlai <svetla_22@yahoo.com>
2014-10-10 03:09:21 +0300

    ProductsCtrl fix.

--- public/app/controllers/product/ProductsCtrl.js
+++ public/app/controllers/product/ProductsCtrl.js
@@ -1,8 +1,8 @@
 /**
  * Created by Виктор on 9.10.2014 г..
  */
-app.controller('ProductsCtrl'['$scope', 'ProductsResource', function ProductsCtrl($scope, ProductsResource) {
-    $scope.products = ProductsResource.query();
-    console.log($scope.products)
+app.controller('ProductsCtrl', ['$scope', 'ProductsResource', function ProductsCtrl($scope, ProductsResource) {
+    $scope.products = ProductsResource();
+    //console.log($scope.products)
     //alert(products);
 }]);
--- public/app/controllers/product/ProductsResource.js
+++ public/app/controllers/product/ProductsResource.js
@@ -2,7 +2,11 @@
  * Created by Виктор on 9.10.2014 г..
  */
 app.factory('ProductsResource', function($resource) {
-    var ProductsResource = $resource('api/products');
+    var ProductsResource = $resource('api/products', {}, {
+        'query': {
+            method: 'GET', isArray: false
+        }
+    });
 
     return ProductsResource;
 })
